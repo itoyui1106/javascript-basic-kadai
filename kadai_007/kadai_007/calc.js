5d4af3ecff929aafa1f33f887d45bb80923a5247
itoyui1106 <itoyui1106@gmail.com>
2024-07-24 21:15:28 +0900

    kadai_007を再々々提出する

--- kadai_007/kadai_007/calc.js
+++ kadai_007/kadai_007/calc.js
@@ -1,22 +1,21 @@
 // 変数(num)に1以上の正の数を代入
-let num = 30;
-
-//変数(num)が3の倍数の場合： “3の倍数です”
-if (num % 3 === 0 && num %5 !== 0) {
-    console.log('3の倍数です');
-}
-
-//変数(num)が5の倍数の場合： “5の倍数です”
-else if (num % 5 === 0 && num %3 !== 0) {
-    console.log('5の倍数です');
-}
+let num = 5;
 
 //変数(num)が3と5の倍数の場合： “3と5の倍数です”
 if (num % 3 === 0 && num % 5 === 0) {
     console.log('3と5の倍数です');
+} 
+
+//変数(num)が3の倍数の場合： “3の倍数です”
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
 }
 
-//それ以外の場合： 変数(num)を出力する
-else{
+//変数(num)が5の倍数の場合： “5の倍数です”
+else if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+
+else {
     console.log(num);
 }
